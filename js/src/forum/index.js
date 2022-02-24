2138dd866d6fdada1005e287e16b012ec3e29fa3
Marco Colia <79002016+justoverclockl@users.noreply.github.com>
2022-02-24 22:48:44 +0100

    Revert "improved UI and changed how locations are stored in database"

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -8,61 +8,84 @@ import UserCard from 'flarum/forum/components/UserCard';
 import Leaflet from 'leaflet';
 
 app.initializers.add('justoverclock/users-map-location', () => {
-  User.prototype.location_country = Model.attribute('location_country');
-  User.prototype.location_countrycode = Model.attribute('location_countrycode');
-  User.prototype.location_postcode = Model.attribute('location_postcode');
-  User.prototype.location_city = Model.attribute('location_city');
-  User.prototype.location_latitude = Model.attribute('location_latitude');
-  User.prototype.location_longitude = Model.attribute('location_longitude');
-  
-  extend(UserCard.prototype, 'infoItems', function (items) {
+  User.prototype.location = Model.attribute('location');
+
+  extend(UserCard.prototype, 'oncreate', function () {
     const user = this.attrs.user;
-    
-    if(user.location_latitude()) {
-      items.add('mapLocation', <div className="location-map location-map-user-profile"/>, -100);
-    }
-  });
-
-  extend(UserCard.prototype, 'oncreate', function (originalResult, vnode) {
-    const user = this.attrs.user;
-    
-    let location = user.location_latitude()
-      ? {
-        lat: user.location_latitude(),
-        lon: user.location_longitude()
-      }
-      : null;
-
-    if (!location) return;
-    
-    let mapElement = vnode.dom.getElementsByClassName('location-map')[0];
-
+    let UserLocation = user.location();
     const publicToken = app.forum.attribute('justoverclock-users-map-location.mapBox-api-key');
-    const markerIconPath = app.forum.attribute('baseUrl') + '/assets/extensions/justoverclock-users-map-location/marker-icon.png';
+    const geocode = 'https://nominatim.openstreetmap.org/search?city=' + UserLocation + '&format=json';
 
-    let markerIcon = L.icon({
-      iconUrl: markerIconPath,
-      iconSize: [25, 41], // size of the icon
-      iconAnchor: [13, 40]
-    });
+    if (UserLocation === '') return;
 
-    let map = L.map(mapElement).setView([location.lat, location.lon], 13);
-    let layerUserCard = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>, Developed by <a href="https://flarum.it/">Marco Colia</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: publicToken,
-    }).addTo(map);
+    let GetCoordinates = fetch(geocode)
+      .then((response) => response.json())
+      .then((coordinates) => {
+        this.latitude = coordinates[0].lat;
+        this.longitude = coordinates[0].lon;
+
+        const markerIconPath = app.forum.attribute('baseUrl') + '/assets/extensions/justoverclock-users-map-location/marker-icon.png';
+
+        let markerIcon = L.icon({
+          iconUrl: markerIconPath,
+          iconSize: [28, 45], // size of the icon
+        });
+
+        let map2 = L.map('map2').setView([this.latitude, this.longitude], 13);
+        let layerUserCard = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>, Developed by <a href="https://flarum.it/">Marco Colia</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: publicToken,
+        }).addTo(map2);
+      });
   });
+  extend(SettingsPage.prototype, 'oncreate', function () {
+    const location = app.session.user.location();
+    const publicToken = app.forum.attribute('justoverclock-users-map-location.mapBox-api-key');
+    const geocode = 'https://nominatim.openstreetmap.org/search?city=' + location + '&format=json';
 
+    if (location === '') return;
+
+    let GetCoordinates = fetch(geocode)
+      .then((response) => response.json())
+      .then((coordinates) => {
+        this.latitude = coordinates[0].lat;
+        this.longitude = coordinates[0].lon;
+
+        const markerIconPath = app.forum.attribute('baseUrl') + '/assets/extensions/justoverclock-users-map-location/marker-icon.png';
+
+        let markerIcon = L.icon({
+          iconUrl: markerIconPath,
+          iconSize: [28, 45], // size of the icon
+        });
+
+        let map = L.map('map').setView([this.latitude, this.longitude], 13);
+        let marker = L.marker([this.latitude, this.longitude], { icon: markerIcon }).addTo(map);
+        let layer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
+            'contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>, ' +
+            'Developed by <a href="https://flarum.it/">Marco Colia</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: publicToken,
+        }).addTo(map);
+      });
+  });
   extend(SettingsPage.prototype, 'settingsItems', function (items) {
     items.add('location', <AddLocationComponent />);
+    items.add('mapDiv', <div className="map-div" id="map" />);
+  });
+  extend(UserCard.prototype, 'infoItems', function (items) {
+    const user = this.attrs.user;
+    let UserLocation = user.location();
+    if (UserLocation === '') return;
+    items.add('mapLocation', <div className="map-div" id="map2" />, -100);
   });
 });
-
-export {
-  AddLocationComponent
-}
